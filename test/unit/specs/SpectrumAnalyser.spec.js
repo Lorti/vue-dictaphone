e282ca1a85ed411e68eb3f51ae89d979616f1f11
Manuel Wieser <office@manuelwieser.com>
2018-05-13 15:43:01 +0200

    Add spectrum analyser unit tests

---
+++ test/unit/specs/SpectrumAnalyser.spec.js
@@ -0,0 +1,28 @@
+import { mount } from '@vue/test-utils';
+import Component from '@/components/SpectrumAnalyser';
+
+describe('SpectrumAnalyser', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Component);
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  it('emits an error and doesn’t render when initialization fails', () => {
+    const wrapper = mount(Component);
+    expect(wrapper.emitted().error).toBeTruthy();
+    expect(wrapper.html()).toBeFalsy();
+  });
+
+  it('renders when there’s a valid spectrum', () => {
+    const wrapper = mount(Component);
+    wrapper.setData({ spectrum: new Uint8Array(null) });
+    expect(wrapper.html()).toMatch(/canvas/);
+  });
+
+  it('calculates points based on the frequency spectrum', () => {
+    const wrapper = mount(Component);
+    wrapper.setData({ spectrum: new Uint8Array([127, 0, 255, 127]) });
+    const expected = [[0, 63.5], [128, 0], [256, 127.5], [384, 63.5]];
+    expect(wrapper.vm.points).toEqual(expect.arrayContaining(expected));
+  });
+});
